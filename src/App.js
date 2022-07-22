78989f7a58729a026478db14ae731a7877155f23
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-22 20:36:37 +0530

    App Progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./styles.css";
-
+import { getPosts } from "./api/axios";
+import { useState, useEffect } from "react";
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    getPosts()
+      .then((json) => {
+        setPosts(json);
+        return json;
+      })
+      .then((json) => {
+        setSearchResults(json);
+      });
+  }, []);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
